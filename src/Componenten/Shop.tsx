1113c6cf51765d8ccdfc7ce955b552786d2b30ab
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-02-25 13:04:48 +0100

    +shuffelArray + style 1.3

--- src/Componenten/Shop.tsx
+++ src/Componenten/Shop.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Grid, Card, CardMedia, CardContent, Typography, CardActions, Button, TextField } from '@mui/material';
+import { Grid, Card, CardMedia, CardContent, Typography, CardActions, Button, TextField, Box } from '@mui/material';
 import { Product, getProducts } from './handleProduct';
 import { useCart } from './CartContext';
 import { useNavigate, useLocation, useSubmit, Form } from 'react-router-dom';
@@ -14,6 +14,7 @@ const StyledContainer = styled.div`
   border-radius: 10px;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
 `;
+
 
 const StyledTextField = styled(TextField)`
   && {
@@ -71,46 +72,49 @@ export default function ProductList() {
 
   return (
     <>
-      <StyledContainer>
-        <Form onSubmit={handleSearch}>
-          <StyledTextField name="search" label="Produkt suchen" variant="outlined" defaultValue={searchTerm} />
-          <StyledButton variant='contained' color='secondary' type="submit">Suchen</StyledButton>
-        </Form>
-      </StyledContainer>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', minHeight: '100vh' }}>
 
-      <Grid container spacing={3} sx={{ maxWidth: '1200px', margin: 'auto', padding: '5px' }}>
-        {products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} key={product.id}>
-            <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '300px', margin: 'auto', height: '100%' }}>
-              <CardMedia
-                component="img"
-                sx={{ width: '80%', height: '200px', objectFit: 'contain', padding: '10px', cursor: 'pointer' }}
-                image={product.image}
-                alt={product.title}
-                onClick={() => handleImageClick(product)}
-              />
-              <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                <Typography gutterBottom variant="h6" component="h2" sx={{ fontSize: '1.25rem' }}>
-                  {product.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
-                  ${product.price}
-                </Typography>
-              </CardContent>
-              <CardActions sx={{ justifyContent: 'center', paddingBottom: '16px' }}>
-                <Button
-                  sx={{ color: 'white', '&:hover': { backgroundColor: '#b3cde0' } }}
-                  onClick={() => handleAddToCart(product)}
-                  variant='contained'
-                  color='secondary'
-                >
-                  Add to cart
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+        <StyledContainer>
+          <Form onSubmit={handleSearch}>
+            <StyledTextField name="search" label="Produkt suchen" variant="outlined" defaultValue={searchTerm} />
+            <StyledButton variant='contained' color='secondary' type="submit">Suchen</StyledButton>
+          </Form>
+        </StyledContainer>
+
+        <Grid container spacing={3} sx={{ maxWidth: '1200px', margin: '0 ,auto', padding: '5px' }}>
+          {products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} key={product.id}>
+              <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '300px', margin: 'auto', height: '100%' }}>
+                <CardMedia
+                  component="img"
+                  sx={{ width: '80%', height: '200px', objectFit: 'contain', padding: '10px', cursor: 'pointer' }}
+                  image={product.image}
+                  alt={product.title}
+                  onClick={() => handleImageClick(product)}
+                />
+                <CardContent sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+                  <Typography gutterBottom variant="h6" component="h2" sx={{ fontSize: '1.25rem' }}>
+                    {product.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
+                    ${product.price}
+                  </Typography>
+                </CardContent>
+                <CardActions sx={{ justifyContent: 'center', paddingBottom: '16px' }}>
+                  <Button
+                    sx={{ color: 'white', '&:hover': { backgroundColor: '#b3cde0' } }}
+                    onClick={() => handleAddToCart(product)}
+                    variant='contained'
+                    color='secondary'
+                  >
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 }
